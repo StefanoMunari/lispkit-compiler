d9bf39ee0682bb5baedd0ef1e26c941666924a20
m0bius <stefanomunari.sm@gmail.com>
2015-12-30 14:55:29 +0100

    ADD: test per "rec_equals"

--- test/parserTest.hs
+++ test/parserTest.hs
@@ -11,7 +11,9 @@ tpt8,
 tpt9,
 tpt10,
 tpt11,
-tpt12
+tpt12,
+tpt13,
+tpt14
 ) 
 where
 
@@ -63,10 +65,23 @@ tpt9 = "let x= lambda (y) y in x(3) end $";
 -- rec_rp -> OK ma Raise in seq_var
 tpt10 = "let x= lambda (y y in x(3) end $";
 
--- TPT10 : F
+-- TPT11 : F
 -- rec_virg -> OK ma Raise in sep_exp
 tpt11 = "let x= lambda (a b c) a+b+c in x(1 2 3) end $";
 
--- TPT11 : T
+-- TPT12 : T
 -- rec_virg
 tpt12 = "let x= lambda (a b c) a+b+c in x(1, 2, 3) end $";
+
+-- TPT13 : F
+-- rec_equals
+tpt13 = "let x lambda (a) a in x(1) end $";
+
+-- TPT14 : T
+-- rec_equals
+tpt14 = "let x = lambda (a) "++
+                    "let y = 3 " ++
+                    "in y+a "++
+                    "end "++
+        "in x(2) "++
+        "end $";
