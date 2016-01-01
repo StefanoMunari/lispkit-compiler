9954904b226dbeeb922ae4540ec5fa67b1ad9174
m0bius <stefanomunari.sm@gmail.com>
2016-01-01 21:29:50 +0100

    FIX: corretto, ora compila

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -1,10 +1,15 @@
 --INTERPRETE SECD COMPLETO in Haskell
 module Interpreter(
-  interprete,
   Valore(..),
+  interprete
 ) 
 where
 
+import Lexer
+import LexerTest
+import SyntaxTest
+import Parser
+import ParserTest
 import Compiler
 
 --------------------------------------------------------------------------------
@@ -13,15 +18,15 @@ import Compiler
 -- tipo che modella gli R-valori delle variabili.
 -- Si tratta dei valori da mettere nella pila S e nell'ambiente dinamico E.
 -- CLO modella le chiusure
-data Valore = V  LKC
+data Valore = V  LKC                    -- prende come argomenti {NUM, BOO, STRI, NIL}
             | OGA                       -- placeholder per l'ambiente ricorsivo
-            | CLO [Secdexpr] [[Valore]]
-            | VLISTA [Valore]
+            | CLO [Secdexpr] [[Valore]] -- chiusura body_funzione ambiente_dinamico
+            | VLISTA [Valore]           -- lista di parametri attuali
             deriving(Show,Eq)
 
 -- datatype per i valori del Dump
-data Dump = CONTR  [Secdexpr] 
-          | TRIPLA [Valore] [[Valore]] [Secdexpr] 
+data Dump = CONTR  [Secdexpr]                     -- controllo : serve per immagazzinare IF THEN ELSE della SEL
+          | TRIPLA [Valore] [[Valore]] [Secdexpr] -- serve per immagazzinare (S E C)
           | DUMMY
           deriving(Show,Eq)
 
@@ -31,11 +36,20 @@ data Dump = CONTR  [Secdexpr]
 
 -- crea l'ambiente dinamico ricorsivo necessario per il trattamento 
 -- della ricorsione. Serve nel caso Rap
+-- []         => produce la lista di parametri vuota
+-- altrimenti => trasforma il parametro "a" tramite lazyClo e continua ricorsivamente sugli altri parametri
 lazyE:: [Valore] -> [Valore] -> [Valore]
 lazyE [] _      = []
 lazyE (a : b) c = lazyClo a c : lazyE b c
 
 -- creazione di una chiusura
+-- c = intera lista dei parametri
+-- CLO => ha trovato una chiusura nella lista dei parametri
+--        a = body
+--        b = E (ambiente dinamico)
+--        => produce una nuova chiusura con :
+--           1) stesso body di prima
+--           2) (tutte le chiusure presenti nella lista rese ricorsive) : (ambiente dinamico)            
 lazyClo:: Valore -> [Valore] -> Valore
 lazyClo (CLO a b) c   = CLO a (lazyE c c : b)
 lazyClo (V x) _       = V x
@@ -55,25 +69,25 @@ locate:: (Integer, Integer) -> [[Valore]] -> Valore
 locate (a, b) e   = index b (index a e)
 
 -- estrae una costante intera
-extract_int :: Value -> Integer
+extract_int :: Valore -> Integer
 extract_int (V (NUM x)) = x
 extract_int x           = error ("trovato altro da intero" ++ (show x))
 
 --funzioni per le liste di Valori VLISTA
 
 -- ritorna il primo elemento della lista
-vhd :: Value -> Value
+vhd :: Valore -> Valore
 vhd (VLISTA (a:b))  = a
 vhd (VLISTA [])     = error "vhd trovata lista vuota"
 vhd _               = error "vhd non trova VLISTA"
 
 -- ritorna la coda della lista
-vtl :: Value -> Value
+vtl :: Valore -> Valore
 vtl (VLISTA (a:b))  = VLISTA b
 vtl (VLISTA [])     = error "vtl trovata lista vuota";
 vtl _               = error "vtl non trova VLISTA"
 
-vatom :: Value -> Value
+vatom :: Valore -> Valore
 vatom (V _)   = V (BOO True)
 vatom _       = V (BOO False)
 
@@ -94,23 +108,26 @@ eqVLISTA (VLISTA (a : b)) (VLISTA (c : d)) = eqValore a c && eqVLISTA (VLISTA b)
 eqVLISTA _ _                               = False
 
 -- test di uguaglianza per valori NON lista
-eqV :: Value -> Value -> Bool
+eqV :: Valore -> Valore -> Bool
 eqV (V a) (V b)   = a==b
 eqV _ _           = False
 
 
 --------------------------------------------------------------------------------
 --Interprete
-
+-- s := stack      (ambiente statico)
+-- e := enviroment (ambiente dinamico)
+-- c := controllo  (esecuzione del codice)
+-- d := dump       (mantiene dump temporaneo di istruzioni)
 interprete:: [Valore] -> [[Valore]] -> [Secdexpr] -> [Dump] -> Valore
 interprete s e c d = case (head c) of 
                                       Ld(b, n) -> let 
                                                       x = (locate (b,n) e)  
                                                   in 
-                                                    interprete (x : s) e (tail c) d
+                                                    interprete (x:s) e (tail c) d
                                       (Ldc k) -> case k of 
-                                                    NIL -> interprete ((VLISTA []):s) e (tail c) d
-		                                                _   -> interprete ((V k):s) e (tail c) d
+                                                          NIL -> interprete ((VLISTA []):s) e (tail c) d
+                                                          _   -> interprete ((V k):s) e (tail c) d
                                       Add -> let 
                                                  operand1 = extract_int (head s)
                                                  operand2 = extract_int (head (tail s))
@@ -144,9 +161,9 @@ interprete s e c d = case (head c) of
                                                  interprete ((V(bool2s_espressione (operand1 <= operand2))):(tail (tail s))) e (tail c)  d
                                       Eq -> case s of 
                                               (w1:w2:w3) -> interprete ((V (bool2s_espressione (eqValore w1 w2))):w3) e (tail c) d
-		                                          _          -> error "manca un argomento in Eq"
-            		                      Car -> interprete ((vhd(head s) ):(tail s)) e (tail c) d
-            		                      Cdr -> interprete ((vtl(head s) ):(tail s)) e (tail c) d
+                                              _          -> error "manca un argomento in Eq"
+                                      Car -> interprete ((vhd(head s) ):(tail s)) e (tail c) d
+                                      Cdr -> interprete ((vtl(head s) ):(tail s)) e (tail c) d
                                       Cons -> case head (tail s) of 
                                                   (VLISTA x)   -> interprete  (VLISTA ((head s):x):(tail (tail s))) e (tail c) d
                                                   x            -> error ("CONS: il secondo argomento non e' una lista" ++ (show  x))
@@ -161,28 +178,31 @@ interprete s e c d = case (head c) of
                                                   (CONTR c1) -> interprete s e c1 (tail d)
                                                   _          -> error "JOIN: il dump non contiene controllo"
                                       Ldf sl -> interprete ((CLO sl e):s) e (tail c) d
-	 	                                  Ap -> case (head s) of
+                                      Ap -> case (head s) of
                                                   (CLO c1 e1) -> case (head (tail s)) of
                                                                      VLISTA x -> interprete [] (x:e1) c1 ((TRIPLA (tail(tail s)) e (tail c)):d)
-                                                  _           -> error "AP senza lista dei parametri"
-                                            _  -> error "AP senza chiusura su s"
+                                                                     _           -> error "AP senza lista dei parametri"
+                                                  _  -> error "AP senza chiusura su s"
 
                                       Rtn ->  case (head d) of 
                                                   (TRIPLA s1 e1 c1) -> interprete ((head s):s1) e1 c1 (tail d)
- 			                                       _ ->  error  "RTN: non trovata TRIPLA su dump"
-       		                            Rap -> case (head s) of
-                                                   (CLO c1 e1) ->  case e1 of
-                                                                        ([OGA]:re)   -> case (head (tail s)) of
-                                                                        (VLISTA vl2) -> interprete [] ((lazyE vl2 vl2):re) c1 ((TRIPLA (tail (tail s)) (tail e) (tail c)):d)
-                                                                        _            -> error "manca [OGA] sull'ambiente di chiusura ric"
-                                                                    _ -> error "non trovata [OGA] nell'ambiente di chiusura ricorsiva"
-            		                              _  -> error "RAP: non trovata chiusura su s"
+                                                  _                 ->  error  "RTN: non trovata TRIPLA su dump"
+                                      Rap -> case (head s) of -- CLO (body, E)
+                                                  (CLO c1 e1) ->  case e1 of --se è una chiusura allora l'ambiente deve iniziare con [OGA] perchè sto controllando le chiusure ricorsive
+                                                                        ([OGA]:re)   -> case (head (tail s)) of --nello stack, sotto la chiusura (CLO) appena controllata, ci deve essere la lista dei parametri attuali
+                                                                                        --invocazione => stack = vuoto, dinamico = lista_parametri_attuali:ambiente_chiusura, controllo = corpo_chiusura, dump = dump dello stato attuale della macchina
+                                                                                        (VLISTA vl2) -> interprete [] ((lazyE vl2 vl2):re) c1 ((TRIPLA (tail (tail s)) (tail e) (tail c)):d)
+                                                                                        --sosituisco ad [OGA] la lista dei parametri attuali resi circolari da lazyE
+                                                                                        _            -> error "manca lista dei parametri dopo OGA";
+                                                                        _            -> error "non trovata [OGA] nell'ambiente di chiusura ricorsiva"
+                                                  _          -> error "RAP: non trovata chiusura su s"
                                       Push -> interprete s  ([OGA]:e)  (tail c)  d
                                       Stop -> (head s)
-            		                      _  -> error "operazione non riconosciuta"
+                                      _  -> error "operazione non riconosciuta"
 
---per facilitare l'uso di interprete
-fin x= (interprete [] [] (x ++ [Stop]) [])  --se x e'  programma Secdexpr da eseguire. Si aggiunge Stop alla fine.
+--se x e'  programma Secdexpr da eseguire. Si aggiunge Stop alla fine.
+execute :: [Secdexpr] -> Valore
+execute secdexprl = interprete [] [] (secdexprl ++ [Stop]) []
 
 -- Mostra che si puo' usare letrec anche con binders non-funzionali. Le var a sinistra non devono apparire a destra
 
@@ -198,12 +218,12 @@ e = "let z=2 in letrec x= 2+z and y= 2*z in x*y*z end end $"
 --corrispondente ad una lista contenuta in Z � la somma dei fattoriali dei suoi elementi: f2=fattoriale, f1=calcola somma dei fattori--ali degli elementi di una
 --lista di interi e f0 distribuisce f1 sulle liste contenute in Z *)
 
-f="letrec f0 = lambda ( x )
-          letrec f1 = lambda(y)
-                 letrec f2=lambda (z) if eq(z , 1) then 1 else z * f2( z - 1 )
-                 in if eq( y , nil ) then 0 else f2 ( car ( y ) ) + f1 ( cdr (y)) end
-          in if eq(x , nil) then nil else cons (f1 ( car ( x )),f0 ( cdr ( x ) ) ) end
-   in f0( cons (cons (3 , cons (3 , nil)), cons( cons (3 , nil), nil))) end $"
+f="letrec f0 = lambda ( x ) "++
+          "letrec f1 = lambda(y) " ++ 
+                 "letrec f2=lambda (z) if eq(z , 1) then 1 else z * f2( z - 1 ) " ++
+                 "in if eq( y , nil ) then 0 else f2 ( car ( y ) ) + f1 ( cdr (y)) end " ++
+          "in if eq(x , nil) then nil else cons (f1 ( car ( x )),f0 ( cdr ( x ) ) ) end " ++
+   "in f0( cons (cons (3 , cons (3 , nil)), cons( cons (3 , nil), nil))) end $"
 
 
 --(* esempio di funzione che restituisce una funzione locale *)
