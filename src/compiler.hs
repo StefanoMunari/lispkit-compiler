7f0645a6daaca4ec7786eba8b9e120c413a5434f
m0bius <stefanomunari.sm@gmail.com>
2016-01-01 14:07:50 +0100

    REFACT: commentato e re-identato codice

--- src/compiler.hs
+++ src/compiler.hs
@@ -8,51 +8,53 @@ where
 
 import Parser
 
-
-
-data Secdexpr = Add 
-              | Sub 
-              |  Mult 
-              | Div 
-              | Rem 
-              | Eq 
-              | Leq 
-              | Car 
-              | Cdr 
-              | Cons 
-              | Atom 
-              | Join 
-              | Rtn 
-              | Stop 
-              | Push 
-              | Ap 
-              | Rap 
-              | Ld (Integer, Integer) 
-              | Ldc LKC
-              | Sel [Secdexpr] [Secdexpr]
-              | Ldf [Secdexpr]
+data Secdexpr = Add                       -- +                        
+              | Sub                       -- -                     
+              | Mult                      -- *  
+              | Div                       -- / 
+              | Rem                       -- % 
+              | Eq                        -- ==
+              | Leq                       -- <= 
+              | Car                       -- testa della lista 
+              | Cdr                       -- coda della lista 
+              | Cons                      -- costruttore di lista  
+              | Atom                      -- identifica argomenti "non lista"  
+              | Join                      -- ripristina il controllo salvato su Dump prima di eseguire Sel [Secdexpr] [Secdexpr]  
+              | Rtn                       -- return : esegue il rispristino della tripla (S E C) dal Dump 
+              | Stop                      -- terminazione : ultima istruzione del programma  
+              | Push                      -- carica un [OGA] in Enviroment  
+              | Ap                        -- applicazione di funzione : corrisponde a CALL
+              | Rap                       -- Ap ricorsiva : sostituisce [OGA] con l'ambiente richiesto dai binder ricorsivi
+              | Ld (Integer, Integer)     -- carica un valore da Enviroment a Stack
+              | Ldc LKC                   -- carica un valore costante {NUM, BOO, STRI, NIL}
+              | Sel [Secdexpr] [Secdexpr] -- condizionale {if then else}
+              | Ldf [Secdexpr]            -- definizione di funzione 
                deriving(Show, Eq)
 
 
 -- funzioni per il calcolo dell'indirizzo di una variabile nell'ambiente
-position::String -> [LKC]-> Integer
-position x [] = error "position: non trova la variabile"
-position x ((VAR z):y) = if z==x then 0 else 1 + (position x y)
-position x _ = error "position: trovata non VAR"
+-- calcola l'indice di una variabile nella lista data
+position:: String -> [LKC] -> Integer
+position _ []           = error "position: non trova la variabile"
+position x (VAR z : y)  = if z == x then 0 else 1 + (position x y)
+position _ _            = error "position: trovata non VAR"
 
-member::String ->[LKC]->Bool
-member x  [] = False
-member x ((VAR z):y) = if x == z then True else member x y
-member x _ = error ("member: trovata non VAR"++ x)
+-- true <-> la variabile è definita
+-- false altrimenti
+member:: String -> [LKC] -> Bool
+member _ []           = False
+member x (VAR z : y)  = if x == z then True else member x y
+member _ _            = error ("member: trovata non VAR"++ x)
 
 -- Integer è l'indice che conta i record di attivazione che stiamo percorrendo
 -- ogni record di attivazione è un lista di nomi
 -- e quindi l'ambiente statico è una lista di liste [[LKC]]
--- viene prodotto il contro dei record di attivazione(Integer)
+-- viene prodotto il conto dei record di attivazione(Integer)
 -- e la posizione all'interno del record(Integer)
-location:: String->Integer-> [[LKC]]-> (Integer, Integer)
-location x _ []= error ("location non trova VAR"++ x)
-location x ct (n:m) =   if (member x n) then (ct, (position x n)) else  (location x (ct+1) m)
+-- calcola l'indice di una variabile in Enviroment
+location:: String -> Integer -> [[LKC]] -> (Integer, Integer)
+location x _ []     = error ("location non trova VAR"++ x)
+location x ct (n:m) = if (member x n) then (ct, position x n) else  location x (ct+1) m
 
 
 sexpr_reverse::[a]->[a]
@@ -60,19 +62,21 @@ sexpr_reverse []= []
 sexpr_reverse (a:b)= (sexpr_reverse b) ++ [a]
 
 
---togliere variabili / espressioni da una lista di Binders
-vars::[(a,b)]->[a]
-vars []= []
-vars ((x,y):r)= (x : (vars r))
+-- toglie espressioni da una lista di Binders
+vars:: [(a, b)] -> [a]
+vars []            = []
+vars ((x, _) : r)  = x : vars r
 
-exprs::[(a,b)]->[b]
-exprs []= []
-exprs((x,y):r)= (y:(exprs r))
+-- toglie variabili da una lista di Binders
+exprs:: [(a, b)] -> [b]
+exprs []           = []
+exprs((_, y) : r)  = y : exprs r
 
--- produrrà una lista di parametri attuali (Secdexpr)
-complist:: [LKC]-> [[LKC]] -> [Secdexpr]->[Secdexpr]
-complist [] _ c = ((Ldc NIL):c)
-complist (x:y) n c = complist y n (comp x n (Cons:c))
+-- compila le istruzioni [LKC] per generare
+-- una lista di parametri attuali [Secdexpr]
+complist:: [LKC]-> [[LKC]] -> [Secdexpr] -> [Secdexpr]
+complist [] _ c    = Ldc NIL : c
+complist (x : y) n c = complist y n (comp x n (Cons : c))
 
 -- il codice viene prodotto da dx a sx, infatti :c
 -- e := programma
@@ -135,33 +139,37 @@ complist (x:y) n c = complist y n (comp x n (Cons:c))
 -- mette su l'ambiente dinamico un RA falso, un [OGA]
 -- Ogni LETREC ha in testa un [OGA] che serve per mettere
 -- a posto gli indirizzi del compilatore quando fa la chiamata ricorsiva
--- serve per fare tornare i conti dei RA
+-- serve per fare tornare i conti dei R
+-- =====================================
+-- Compila un programma LKC in una sequenza di comandi SECD eseguibili dall'interprete
 comp:: LKC -> [[LKC]] -> [Secdexpr]->[Secdexpr]
-comp e n c =  case e of (VAR x) -> ((Ld (location x 0 n)):c)
-                        (NUM x)-> (Ldc (NUM x)):c
-                        (BOO x)-> (Ldc (BOO x)):c
-                        (STRI x)-> (Ldc (STRI x)):c
-                        NIL -> (Ldc NIL):c
-                        (ADD x y)-> comp y n (comp x n (Add:c))
-                        (SUB x y)-> comp y n (comp x n (Sub:c))
-                        (MULT x y)-> comp y n (comp x n (Mult:c))
-                        (DIV x y)-> comp y n (comp x n (Div:c))
-                        (REM x y)-> comp y n (comp x n (Rem:c))
-                        (EQC x y)-> comp y n (comp x n (Eq:c))
-                        (LEQC x y)-> comp y n (comp x n (Leq:c))
-                        (CARC x)-> comp x n (Car:c)
-                        (CDRC x)-> comp x n (Cdr:c)
-                        (CONSC x y)-> comp y n (comp x n (Cons:c))
-                        (ATOMC x)-> comp x n (Atom:c)
-                        (IFC x y z)-> let thenp=(comp y n [Join])
-                                          elsep=(comp z n [Join])
-                                      in comp x n  ((Sel thenp elsep):c)
-                        (LAMBDAC x y)-> (Ldf (comp y (x:n) [Rtn])):c
-                        (LETC x y)->  --DA FARE
+comp e n c =  case e of 
+                        (VAR x)       -> ((Ld (location x 0 n)):c)
+                        (NUM x)       -> (Ldc (NUM x)):c
+                        (BOO x)       -> (Ldc (BOO x)):c
+                        (STRI x)      -> (Ldc (STRI x)):c
+                        NIL           -> (Ldc NIL):c
+                        (ADD x y)     -> comp y n (comp x n (Add:c))
+                        (SUB x y)     -> comp y n (comp x n (Sub:c))
+                        (MULT x y)    -> comp y n (comp x n (Mult:c))
+                        (DIV x y)     -> comp y n (comp x n (Div:c))
+                        (REM x y)     -> comp y n (comp x n (Rem:c))
+                        (EQC x y)     -> comp y n (comp x n (Eq:c))
+                        (LEQC x y)    -> comp y n (comp x n (Leq:c))
+                        (CARC x)      -> comp x n (Car:c)
+                        (CDRC x)      -> comp x n (Cdr:c)
+                        (CONSC x y)   -> comp y n (comp x n (Cons:c))
+                        (ATOMC x)     -> comp x n (Atom:c)
+                        (IFC x y z)   -> let 
+                                            thenp = comp y n [Join]
+                                            elsep = comp z n [Join])
+                                         in comp x n  ( Sel thenp elsep : c)
+                        (LAMBDAC x y) -> (Ldf (comp y (x:n) [Rtn])):c
+                        (LETC x y)    ->  --DA FARE
 
-                        (LETRECC x y)-> --DA FARE
-                        (CALL x y)-> complist y n (comp x n (Ap:c))
-                        _ -> [];
+                        (LETRECC x y) -> --DA FARE
+                        (CALL x y)    -> complist y n (comp x n (Ap:c))
+                        _             -> [];
 
 
 --esempi di prova
