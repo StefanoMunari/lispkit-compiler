f09a15b98f36ad28b977694666575af6c5a40cf6
m0bius <stefanomunari.sm@gmail.com>
2016-05-25 04:44:59 +0200

    tidying-up tests

--- test/lexerTest.hs
+++ test/lexerTest.hs
@@ -1,92 +1,68 @@
--- LEXER: ==== TEST UNITA ====
--- @tofix: manca implementazione dell'operatore di modulo, indicato in parser.hs
--- con REM
+-- == TEST FOR Lexer MODULE ==
 module LexerTest (
-ltu0,
-ltu1,
-ltu2,
-ltu3,
-ltu4,
-ltu5,
-ltu6,
-ltu7,
-ltu8,
-ltu9,
-ltu10,
-ltu11,
-ltu12,
-ltu13,
-ltu14,
-ltu15,
-ltu16,
-ltu17,
-ltu18,
-ltu19
-) 
+l0,
+l1,
+l2,
+l3,
+l4,
+l5,
+l6,
+l7,
+l8,
+l9,
+l10,
+l11,
+l12,
+l13,
+l14,
+l15,
+l16,
+l17,
+l18,
+l19
+)
 where
 
--- == TEST SUI CASI DI PATTERN MATCHING ==
--- TU0 : test su programma malformato : F
-ltu0 = "";
+-- == TEST ON PATTERN MATCHING ==
+l0 = "";
 
--- TU1 : test su programma vuoto : T
-ltu1 = "$"
+l1 = "$"
 
--- TU2 : test su programma con spazi : T
-ltu2 = "      $"
+l2 = "      $"
 
--- TU3 : test su programma con altre tipologie di spazi : T
-ltu3 = "\t \n \f \t$"
+l3 = "\t \n \f \t$"
 
--- TU4 : test su programma con spazi malformato : F
-ltu4 = "   \n  "
+l4 = "   \n  "
 
--- == TEST SUI CASI DI n ==
--- ** TEST SU ~n **
+--  n
+l5 = "let x = ~ $";
 
--- TU5 : test su programma malformato per n : F
-ltu5 = "let x = ~ $";
+l6 = "let y = 5340 $";
 
--- ** TEST SU n **
--- TU6 : test su programma corretto per n : T
-ltu6 = "let y = 5340 $";
+-- sc
+l7 = " \"\"\" $";
 
--- == TEST SUI CASI DI sc ==
--- TU7 : test su programma errato per sc : F
-ltu7 = " \"\"\" $";
+l8 = " \" sdfs~~~dfsd gfh 5 thwdTRHGFG-F$\" $";
 
--- TU8 : test su programma corretto per sc : T
-ltu8 = " \" sdfs~~~dfsd gfh 5 thwdTRHGFG-F$\" $";
+l9 = " let y = \"pippo\" in y $";
 
--- TU9 : test su programma corretto per sc : T
-ltu9 = " \" sdfs~~~dfsd gfh 5 thwdTRHGFG-F$\" $";
+l10 = " \"~  $";
 
--- TU10 : test su programma corretto per sc : F
-ltu10 = " \"~  $";
+l11 = " \"\"$";
 
--- TU11 : test su programma corretto per sc : T
-ltu11 = " \"\"$";
+l12 = "()=+-*/$";
 
--- TU12 : test su programma corretto per symbol : F
-ltu12 = "()=+-*/$";
+l13 = "() $";
 
--- TU13 : test su programma corretto per symbol : T
-ltu13 = "() $";
+-- s
+l14 = "abcds $";
 
--- TU14 : test su programma corretto per s : T -> variabile/identificatori
-ltu14 = "abcds $";
+l15 = "t asdc $";
 
--- TU15 : test su programma corretto per s : T -> variabili/identificatori
-ltu15 = "t asdc $";
+l16 = "True and False $";
 
--- TU16 : test su programma corretto per s : T
-ltu16 = "True and False $";
+l17 = "True and False ";
 
--- TU17 : test su programma corretto per s : F
-ltu17 = "True and False ";
+l18 = "T 0";
 
--- TU18 : test su programma errato : F -> malformed input
-ltu18 = "T 0";
-
--- TU19 : test su programma corretto per n : T
-ltu19 = "let k = (~233 + 10) $";
+l19 = "let k = (~233 + 10) $";
